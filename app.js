959a30a0abaa34f8fd02ae49d0499aa51f3e9625
ynitin2906 <ynitin2906@gmail.com>
2023-05-30 18:38:50 +0530

    code

--- app.js
+++ app.js
@@ -5,29 +5,35 @@ const mongoose = require("mongoose");
 require("./db/conn");
 const cookieParser = require("cookie-parser");
 
-
 const Products = require("./models/productsSchema");
 const DefaultData = require("./defaultdata");
 const cors = require("cors");
 const router = require("./routes/router");
-
 
 app.use(express.json());
 app.use(cookieParser(""));
 app.use(cors());
 app.use(router);
 
-
 const port = process.env.PORT || 8005;
 
-// for deployment
-if(process.env.NODE_ENV === "production"){
-    app.use(express.static("client/build"))
-}
+// // for deployment
+// if(process.env.NODE_ENV === "production"){
+//     app.use(express.static("client/build"))
+// }
 
-app.listen(port,()=>{
-    console.log(`server is running on port number ${port}`);
+app.use(express.static(path.join(__dirname, "./client/build")));
+app.get("*", function (_, res) {
+  res.sendFile(
+    path.join(__dirname, "./client/build/index.html"),
+    function (err) {
+      res.status(500).send(err);
+    }
+  );
 });
 
+app.listen(port, () => {
+  console.log(`server is running on port number ${port}`);
+});
 
 DefaultData();
